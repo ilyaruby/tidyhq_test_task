8df667ceb277c935647aaccc6f9b25e75f73484e
Ilya Timofeev <ilya_ruby@fastmail.com>
2024-05-10 03:24:24 +0400

    a simple JSON API

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class LinksController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
+      def create
+        link = Link.new(link_params)
+        if link.save
+          render json: { short_url: link.short_url, original_url: link.original_url }, status: :created
+        else
+          render json: link.errors, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        link = Link.find_by(short_url: params[:short_url])
+        if link
+          render json: { original_url: link.original_url }
+        else
+          render json: { error: "Not found" }, status: :not_found
+        end
+      end
+
+      private
+
+      def link_params
+        params.require(:link).permit(:original_url)
+      end
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :links, only: [:new, :create]
   get '/links/:short_url', to: 'links#show', as: :shortened
   get '/s/:short_url', to: 'links#unshort', as: 'unshort'
+
+	namespace :api do
+		namespace :v1 do
+			resources :links, only: [:create, :show], param: :short_url
+		end
+	end
 end
